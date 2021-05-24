130bd84c36ccd4ef40c02fb239302c127588a069
Abyalew Ambaneh <77543474+AbyalewAmbaneh@users.noreply.github.com>
2021-05-23 21:49:17 -0500

    Add files via upload

---
+++ HomeWork_02/Problem_02/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("port", 5000);
+
+const server = app.listen(app.get("port"), function () {
+  console.log("Listening to port " + app.get("port"));
+});
+
+app.get("/", function (req, res) {
+  console.log("File request received");
+  res.status(200).sendFile(path.join(__dirname, "public", "index.html"));
+});
